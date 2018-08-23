38b2c09cedf1bf205a60a1e51fa9cd9e1a0b5bb1
Antoine07 <antoine.lucsko@gmail.com>
2018-08-23 10:29:04 +0200

    fix bug comment title

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Comment } from '../comments';
 import { CommentsService } from '../comments.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-comments',
@@ -9,12 +10,14 @@ import { CommentsService } from '../comments.service';
 })
 export class CommentsComponent implements OnInit {
 
-  comments : Comment[] = [];
+  comments: Comment[] = [];
+  title_component: string;
 
-  constructor(private cS : CommentsService) { }
+  constructor(private route: ActivatedRoute, private cS: CommentsService) { }
 
   ngOnInit() {
     this.comments = this.cS.getComments();
+    this.title_component = this.route.snapshot.data.title_component;
   }
 
 }
